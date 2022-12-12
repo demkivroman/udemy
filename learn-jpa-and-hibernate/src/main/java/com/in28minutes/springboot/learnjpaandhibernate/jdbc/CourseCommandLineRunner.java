d3066305dc73ca9d2f13815776718f2d70748226
Demkiv, Roman [PS] <roman.demkiv@cengage.com>
2022-12-12 09:53:24 +0200

    add new more one functionality

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/jdbc/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/jdbc/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.jdbc;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,22 +10,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 //    private CourseJdbcRepository repository;
-    private CourseJpaRepository repository;
+//    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Autowired
-    public CourseCommandLineRunner(CourseJpaRepository repository) {
+    public CourseCommandLineRunner(CourseSpringDataJpaRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Java Clean Code jpa", "Roman Demkiv"));
-        repository.insert(new Course(2, "Java Clean Code2 jpa", "Roman Demkiv"));
-        repository.insert(new Course(3, "Java Dev Ops jpa", "Roman Demkiv"));
+        repository.save(new Course(1, "Java Clean Code jpa", "Roman Demkiv"));
+        repository.save(new Course(2, "Java Clean Code2 jpa", "Roman Demkiv"));
+        repository.save(new Course(3, "Java Dev Ops jpa", "Roman Demkiv"));
 
-        repository.deleteById(1);
+        repository.deleteById(1l);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println("Results\n");
+        System.out.println("======================================");
+
+        System.out.println(repository.findById(2l));
+        System.out.println(repository.findById(3l));
+        System.out.println(repository.findAll());
+        System.out.println(repository.findByAuthor("Roman Demkiv"));
     }
 }
---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.in28minutes.springboot.learnjpaandhibernate.repository;
+
+public interface UserRepository {
+}
